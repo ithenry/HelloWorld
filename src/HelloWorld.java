69bbaf66fbac30762b21d936634452c54406d750
ithenry <55816847+ithenry@users.noreply.github.com>
2019-09-29 15:12:50 -0700

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+//HEADER
+//Program Name: Hello World!
+//Author: Isaak Henry
+//Class CS260 Fall 2019
+//Date: 09/29/2019
+// The problem that this program solves, is that there is no
+// Hello World! being printed. This program prints Hello World!
+public class HelloWorld {
+	//Do not want a return, and using a string as an argument
+	public static void main(String[] args) {
+		//Print out Hello World
+		System.out.println("Hello World!");
+
+	}
+
+}
